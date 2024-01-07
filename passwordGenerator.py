fbc3121e3c1733dabe2f158ac3d42bda209f72da
Ananya Maheshwari <ananyamaheshwari2004@gmail.com>
2024-01-07 22:30:44 +0530

    logic improved

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -7,25 +7,32 @@ yes_input = ["y", "Y", "yes", "YES"]
 
 length = int(input("Enter minimum password length: "))
 include_numbers = input("Enter y if you want digits in your password: ") in yes_input
+include_cap = (
+    input("Enter y if you want a capital letter in your password: ") in yes_input
+)
 include_special = (
     input("Enter y if you want special characters in your password: ") in yes_input
 )
 
 
-def generatePass(min_length, special_chars=True, number_chars=True):
-    letters = string.ascii_letters
+def generatePass(min_length, special_chars=True, number_chars=True, cap_chars=True):
+    lower_letters = string.ascii_lowercase
+    upper_letters = string.ascii_uppercase
     digits = string.digits
     special = string.punctuation
 
-    characters = letters
+    characters = lower_letters
     if number_chars:
         characters += digits
     if special_chars:
         characters += special
+    if cap_chars:
+        characters += upper_letters
 
     pwd = ""
     hasNumber = False
     hasSpecial = False
+    hasCap = False
     criteria_met = False
 
     while len(pwd) < min_length or not criteria_met:
@@ -36,16 +43,18 @@ def generatePass(min_length, special_chars=True, number_chars=True):
             hasNumber = True
         elif newChar in special:
             hasSpecial = True
+        elif newChar in upper_letters:
+            hasCap = True
 
-
-        if special_chars and hasSpecial:
-            criteria_met = True
-        elif not special_chars and number_chars and hasNumber:
-            criteria_met =True
-        if number_chars and hasNumber:
-            criteria_met = criteria_met and True
+        criteria_met = True
+        if special_chars:
+            criteria_met = hasSpecial
+        if number_chars:
+            criteria_met = criteria_met and hasNumber
+        if cap_chars:
+            criteria_met = criteria_met and hasCap
 
     print("Your Password is: ", pwd)
 
 
-generatePass(length, include_special, include_numbers)
+generatePass(length, include_special, include_numbers, include_cap)
