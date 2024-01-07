ed2de56180a1f5b5af0fc21b8c7c67c438593d9c
Ananya Maheshwari <ananyamaheshwari2004@gmail.com>
2024-01-07 20:49:33 +0530

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,7 +5,7 @@ print("Welcome to Password Generator")
 
 yes_input = ["y", "Y", "yes", "YES"]
 
-length = int(input("Enter password length: "))
+length = int(input("Enter minimum password length: "))
 include_numbers = input("Enter y if you want digits in your password: ") in yes_input
 include_special = (
     input("Enter y if you want special characters in your password: ") in yes_input
@@ -37,8 +37,11 @@ def generatePass(min_length, special_chars=True, number_chars=True):
         elif newChar in special:
             hasSpecial = True
 
+
         if special_chars and hasSpecial:
             criteria_met = True
+        elif not special_chars and number_chars and hasNumber:
+            criteria_met =True
         if number_chars and hasNumber:
             criteria_met = criteria_met and True
 
